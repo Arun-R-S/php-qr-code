add3f3ff2442020926e91725d38bd041e46ea765
ztorq-project <arunrs0966@gmail.com>
2022-01-03 13:46:24 +0530

    first commit

---
+++ v1/index.php
@@ -0,0 +1,25 @@
+<?php
+
+include('library/qrlib.php');
+    
+    // outputs image directly into browser, as PNG stream
+    QRcode::png('This is build by anderson',false,QR_ECLEVEL_Q,16,3);
+
+/*    include('library/qrlib.php');
+    //include('config.php');
+
+    // how to save PNG codes to server
+    
+    $tempDir = "C:/xampp/htdocs/php qr code/temp";
+    
+    $codeContents = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a';
+    
+    // generating
+    //QRcode::png($codeContents, $tempDir.'/006_L.png', QR_ECLEVEL_L);
+    //QRcode::png($codeContents, $tempDir.'/006_M.png', QR_ECLEVEL_M);
+    //QRcode::png($codeContents, $tempDir.'/006_Q.png', QR_ECLEVEL_Q);
+    QRcode::png($codeContents, $tempDir.'/006_H.png', QR_ECLEVEL_H);
+
+    echo '<img src="temp/006_H.png" />'; */
+
+?>
---
+++ v1/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '64ba608f1652357b7611da279ef77a501e142792',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '64ba608f1652357b7611da279ef77a501e142792',
+            'dev_requirement' => false,
+        ),
+    ),
+);
